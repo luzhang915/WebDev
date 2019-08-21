09bbeadb6b769c01a3ea3c7ed5fc7fb4316a441e
Lu Zhang <luz20@pitt.edu>
2019-08-21 14:27:37 +0800

    dynamically render json objects(users) from server

--- src/main/webapp/user-admin.controller.client.js
+++ src/main/webapp/user-admin.controller.client.js
@@ -15,5 +15,42 @@
     
     function renderUsers(users) {
         console.log(users);
+
+        var tbody = $('tbody');
+        tbody.empty();
+        for (var i=0; i<users.length; i++) {
+            var user = users[i];
+
+            var tr = $('<tr>');
+            var td = $('<td>');
+            td.append(user.username);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append(user.password);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append(user.firstname);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append(user.lastname);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append('hello@world.com');
+            tr.append(td);
+
+            td = $('<td>');
+            td.append('Student');
+            tr.append(td);
+
+            td = $('<td>');
+            var deleteBtn = $('<button>DELETE</button>');
+            td.append(deleteBtn);
+            tr.append(td);
+            tbody.append(tr);
+        }
     }
 })();
